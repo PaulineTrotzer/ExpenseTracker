73c3257e2ff38e899a088876b0ebb1afa69ad562
Pauline Trotzer <147944434+PaulineTrotzer@users.noreply.github.com>
2025-04-11 16:23:47 +0200

    states and childrens

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
-function App(){
-  return <div><ListGroup/></div>
+function App() {
+  /*   let items = ["London", "Paris", "Lisbon", "New York", "Barcelona"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }; */
+
+  /*   return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  ); */
+
+  return (
+    <div className="alert alert-primary">
+      <Alert>Hello World</Alert>
+    </div>
+  );
 }
 
 export default App;
