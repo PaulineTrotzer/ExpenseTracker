884de2ef97aa706977ffa31a10cb4e8b76dd5b05
Pauline Trotzer <147944434+PaulineTrotzer@users.noreply.github.com>
2025-04-13 20:31:36 +0200

    using css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import Button from "./components/Button";
-import Alert from "./components/Alert";
 import { useState } from "react";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  /*   const [alertVisible, setAlertVisibility] = useState(false); */
+  const items = ["New York", "Miami", "San Francisco"];
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>
-        My Button
-      </Button>
+      <ListGroup
+        items={items}
+        heading={"Miami"}
+        onSelectItem={() => {}}
+      ></ListGroup>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.css';
-import './components/ListGroup.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+/* import "bootstrap/dist/css/bootstrap.css"; */
+import "./components/ListGroup/ListGroup";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
